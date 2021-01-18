3ce9a9954589709c69769a457b1ab2fe18acca5c
TeppeiMatsuda73 <i.w.b.zoo.zoo.sea.neko.no.10@gmail.com>
2021-01-18 19:08:52 +0900

    pythonmemo

--- ABC/PythonMemo.py
+++ ABC/PythonMemo.py
@@ -2,10 +2,20 @@
 list = (1, 2, 3, 4, 5, 6)
 
 # 最小値を求める
-print(min(list))
+min(list)  # --> 1
 
 # 最大値を求める
-print(max(list))
+max(list)  # --> 6
 
 # 配列のサイズを求める
-print(len(list))
+len(list)  # --> 6
+
+# リストをn次元で格納(線形代数ってやつ？)
+# extend_list = [list(map(int, input().split())) for i in range(n)]
+
+# リストへの要素追加
+ex_list = [1, 3]
+# リストそのものが追加される
+ex_list.append([5, 7, 9])  # --> [1, 3, [5, 7, 9]]
+# リスト内の要素が追加される
+ex_list.extend([4, 6, 8])  # --> [1, 3, [5, 7, 9], 4, 6, 8]
