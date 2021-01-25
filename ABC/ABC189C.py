11a67c6f6d2600998d106f25c0a318f98968acc6
TeppeiMatsuda73 <i.w.b.zoo.zoo.sea.neko.no.10@gmail.com>
2021-01-25 23:39:15 +0900

    210125

--- ABC/ABC189C.py
+++ ABC/ABC189C.py
@@ -1,25 +1,23 @@
+# TLEで通らない O(n^2)
+# n = int(input())
+# a = list(map(int, input().split()))
+# ans = 0
+# for i in range(len(a)):
+#     for j in range(i, len(a)):
+#         a_min = min(a[i:j+1])
+#         total = a_min * (j - i + 1)
+#         if total > ans:
+#             ans = total
+# print(ans)
+
 n = int(input())
 a = list(map(int, input().split()))
-a_sub = list(set(a))
-sorted(a_sub, reverse=True)
-for i in range(len(a_sub)):
-    for j in range(i+1, len(a_sub)-1):
-        if a_sub[i] >= a_sub[j]*n:
-            a_sub.remove(a_sub[j])
+ans = 0
+for i in range(len(a)):
+    for j in range(i, len(a)):
+        a_min = min(a[i:j+1])
+        total = a_min * (j - i + 1)
+        if total > ans:
+            ans = total
+print(ans)
 
-max_orange = 0
-for i in a_sub:
-    total = 0
-    for j in a:
-        if i <= j:
-            total += i
-
-        if total != 0 and i > j:
-            if max_orange < total:
-                max_orange = total
-                total = 0
-        if max_orange < total:
-            max_orange = total
-
-
-print(max_orange)
